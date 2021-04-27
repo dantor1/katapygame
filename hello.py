783dbd823b306a7839ab3536241b075da5df3d08
Daniel <torrcdaniel@gmail.com>
2021-04-28 00:20:35 +0200

    Programa con varias bolas

--- hello.py
+++ hello.py
@@ -1,4 +1,9 @@
 import pygame as pg
+import sys
+
+def fin_juego():
+    pg.quit()
+    sys.exit()
 
 pg.init() # Inicializamos todos los sistemas de pygame
 pantalla = pg.display.set_mode((600, 400))
@@ -9,10 +14,15 @@ game_over = False
 while not game_over:
     #Gestión de eventos
     for evento in pg.event.get():
-        pass
+        if evento.type == pg.QUIT:
+            game_over = True
+
     #Gestión del estado (que si la bola, que si el personaje pierde la vida, etc)
     print('Hola mundo')
+    
     # Refrescar pantalla
     pantalla.fill((0, 255, 0))
     pg.display.flip() # Esta instrucción es la última de refrescar pantalla
+
+    
 
